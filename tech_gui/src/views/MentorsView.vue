0b45d561a007594a568b76ad497fa6c0f0800fc2
siyamanga22 <zwenisiyamanga@gmail.com>
2023-08-31 16:24:53 +0200

    done

--- tech_gui/src/views/MentorsView.vue
+++ tech_gui/src/views/MentorsView.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="mentors">
 <h2>Mentors Page</h2>
+<div v-for='Career in careers' class="card" style="width: 18rem;" :key="Career.careerID">
+    <img :src="Career.image" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">{{Career.describtion}}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        computed: {
+    careers() {
+      return this.$store.state.careers;
+    }
+  },
+  mounted() {
+    this.$store.dispatch('fetchCareers');
+  }
     }
 </script>
 
